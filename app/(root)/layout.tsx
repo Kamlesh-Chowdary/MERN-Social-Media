cc528d82d1568bc4aa11a2754e4ae8f3ee6c729b
himiaaaaa <126934346+himiaaaaa@users.noreply.github.com>
2023-10-25 21:10:32 +0300

    homepage

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,11 +3,16 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
 
+import Topbar from "@/components/shared/Topbar";
+import Bottombar from "@/components/shared/Bottombar";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import RightSidebar from "@/components/shared/RightSidebar";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Stitch",
-  description: "A Next.js 13 Meta Threads App.",
+  description: "A Next.js 13 social media App.",
 };
 
 export default function RootLayout({
@@ -18,7 +23,23 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+
+            <main className="flex flex-row">
+              <LeftSidebar />
+
+              <section className="main-container">
+                <div className="w-full max-w-4xl">
+                  {children}
+                </div>
+              </section>
+
+              <RightSidebar />
+            </main>
+
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
